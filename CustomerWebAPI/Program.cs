91ebcc58bb10d69d716af0da47ddd70f3545aabb
Stipe_Cule <stipe.cule@socius.hr>
2022-10-13 17:39:07 +0200

    latest

--- CustomerWebAPI/Program.cs
+++ CustomerWebAPI/Program.cs
@@ -7,10 +7,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-var dbHost = @"(LocalDb)\MSSQLLocalDB";
-var dbName = "dms_customer";
-var dbPassword = "P@ssw0rd121#";
-var connectionString = $"Data Source={dbHost}; Initial Catalog={dbName}; Integrated Security=True;";
+var dbHost = Environment.GetEnvironmentVariable("DB_HOST");
+var dbName = Environment.GetEnvironmentVariable("DB_NAME");
+var dbPassword = Environment.GetEnvironmentVariable("DB_SA_PASSWORD");
+var connectionString = $"Data Source={dbHost}; Initial Catalog={dbName}; User ID=sa; Password={dbPassword}";
 builder.Services.AddDbContext<CustomerDbContext>(opt => opt.UseSqlServer(connectionString));
 
 var app = builder.Build();
